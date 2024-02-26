00ff538cecd77f179cfe41049eaf9ec5b027137c
ismail-lafhiel <lafhiel.ismail.contact@gmail.com>
2024-02-26 18:00:46 +0100

    TAS-1 finished emplimenting sanctum authentication system

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            "name" => "required|string",
+            "email" => "required|unique:users,email",
+            "password" => "required|string|confirmed",
+        ]);
+
+        $user = User::create([
+            "name" => $fields['name'],
+            "email" => $fields['email'],
+            "password" => bcrypt($fields['password']),
+        ]);
+
+        $token = $user->createToken('mytoken')->plainTextToken;
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response()->json($response, 201);
+    }
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            "email" => "required",
+            "password" => "required|string",
+        ]);
+
+        $user = User::where("email", $fields["email"])->first();
+
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+        $token = $user->createToken('mytoken')->plainTextToken;
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response()->json($response, 201);
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'log out'
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// private route
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+
+// public route
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
